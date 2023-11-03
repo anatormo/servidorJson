7be12d266464b62b50ce4f7e00db3fd484b4e5db
anatormodominguez <220060@iessanalberto.com>
2023-11-03 18:14:37 +0100

    second

--- src/componentes/ListUser.js
+++ src/componentes/ListUser.js
@@ -1,49 +1,55 @@
-import { useState, useEffect } from 'react';
 
-import React from 'react'
+import { useState, useEffect } from 'react';
 
 const url = "http://localhost:5000/users";
 
-/*funcion buscarPublicaiones, necesaria para extraer los datos de la url que le pasamos*/
+/*funcion buscarPublicaiones, 
+necesaria para extraer los datos de la url que le pasamos*/
 async function fetchPosts() {
-    const respuesta = await fetch(url);
-    const infoUser = await respuesta.json();
-    return infoUser;
-  }
+  const response = await fetch(url);
+  const infoUser = await response.json();
+  return infoUser;
+}
 
-  function ListUser() {
-    /*almacena la lista de ususrios del servidor */
-    const [cargaUser, setCargaUser] = useState([]);
+function ListUser() {
+  /*almacena la lista de ususrios del servidor */
+  const [cargaUser, setCargaUser] = useState([]);
+  
+  /*carga la lista de usuarios del servidor, pasamos la funcion fetchPosts
+  y userBuscado acualiza el estado de cargarUser */
+  useEffect(() => {
+    fetchPosts().then((userBuscado) => setCargaUser(userBuscado));
+  }, []);
 
-    /*carga la lista de usuarios del servidor, pasamos la funcion fetchPosts
-    y userBuscado acualiza el estado de cargarUser */
-    useEffect(function () {
-      fetchPosts().then((userBuscado) => setCargaUser(userBuscado));
-    }, []);
-
-
-    return (
-      <div>
+/*almacena los id de ususrios del servidor */
+const [selectUser, setSelectUser] = useState();
+/*funcion para ver los id de ususrios del servidor */
+  function verUser (user) {
+    setSelectUser(user);
+  };
+  /*el return recorre la lista de los usuarios y le pasamos el user de la 
+  funcion verUser para que nos de la info del usuario seleccionado */
+  return (
+    <div>
+      <ul>
+        {cargaUser.map((user) => (
+          <li key={user.id}>
+            {user.name}
+            <button onClick={() => verUser(user)}>Mostrar todos</button>
+          </li>
+        ))}
+      </ul>
+      {selectUser && (
+        <div>
           <ul>
-          {cargaUser.map((post) => (
-            <li key={post.id}>{post.name}  
+            <li>{selectUser.name}</li>
+            <li>{selectUser.phone}</li>
+            <li>{selectUser.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
 
-
-            <button onClick={()=>{
-              // console.log(post);
-              <ul>
-                <li>{post.name}</li>
-                <li>{post.phone}</li>
-                <li>{post.email}</li>
-              </ul>  
-            }}>Mostrar todos</button>
-            
-            </li>
-          ))}
-        </ul>
-      </div>
-  
-    );
-  }
-  
-  export default ListUser;
+export default ListUser;
